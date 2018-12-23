2d978b34042a56b2f739956c1990519eea442c6e
Horhof <hor@hof.hh>
2018-12-23 11:57:06 -0500

    Add CLI

---
+++ Cli.ts
@@ -0,0 +1,21 @@
+/**
+ * Generate the combinations for a two-word phrase separated by a space.
+ *
+ * Usage:
+ *
+ *     $ ts-node Cli "Han Solo"
+ *     Holo San
+ *     Hlo Soan
+ *     Ho Solan
+ *     Haolo Sn
+ *     Halo Son
+ *     Hao Soln
+ */
+
+import { Phrase } from './src/Phrase'
+
+/** Isolate arg from arguments ['node', 'Cli', arg]. */
+const arg = process.argv[2]
+const p = new Phrase(arg)
+const results = p.generateCombinations()
+for (const result of results) console.log(result)
