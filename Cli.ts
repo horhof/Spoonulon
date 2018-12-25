27ba520108d7e7a8cc75f2a7318aaadd28dd5213
Horhof <hor@hof.hh>
2018-12-25 02:30:40 -0500

    Handle Phrase#generate errors in Cli

--- Cli.ts
+++ Cli.ts
@@ -15,4 +15,14 @@ import { Phrase } from './src/Phrase'
 const arg = process.argv[2]
 const p = new Phrase(arg)
 const results = p.generate()
-for (const result of results) console.log(result)
+if (results instanceof Error) {
+  console.error(`Error: "${arg}" is an invalid phrase.`)
+  process.exit(1)
+}
+else {
+  if (results.length === 0) {
+    console.error(`Error: "${arg}" has no available results.`)
+    process.exit(1)
+  }
+  for (const result of results) console.log(result)
+}
