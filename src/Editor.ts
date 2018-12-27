26cd29e4d9e58a124a69af53f8b1348d72222027
Horunabu Hofutenho <horhof@protonmail.com>
2018-12-27 10:45:08 -0500

    Add web UI

---
+++ src/Editor.ts
@@ -0,0 +1,38 @@
+import { Phrase, PhraseError } from './Phrase'
+import * as Debug from 'debug'
+
+const debug = Debug('Spoonulon:Editor')
+
+export class Editor {
+  input = ``
+
+  output: string[] | undefined
+
+  haveResults = false
+
+  valid = false
+
+  phrase = new Phrase()
+
+  constructor($location: any) {
+    const phrase = $location.hash()
+    if (phrase) {
+      this.input = phrase
+      this.update()
+    }
+  }
+
+  update() {
+    debug(`Update>`)
+    const results = this.phrase.generate(this.input)
+    if (results instanceof Error) {
+      this.valid = false
+      this.output = undefined
+    }
+    else {
+      this.valid = true
+      this.output = results
+      this.haveResults = results.length > 0
+    }
+  }
+}
