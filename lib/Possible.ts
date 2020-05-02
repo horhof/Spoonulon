0fee32468ca4e778f643739080606aa51168f630
Horunabu Hofutenho <horhof@protonmail.com>
2020-05-01 21:55:36 -0400

    Remove previous implementation

--- lib/Possible.ts
+++
@@ -1,9 +0,0 @@
-import { Failure } from './Failure'
-
-/**
- * A Possible is the return value of a function that either succeeds and
- * returns void or fails and returns a Failure.
- *
- * Version 1.0.0
- */
-export type Possible<T> = Failure<T> | void
